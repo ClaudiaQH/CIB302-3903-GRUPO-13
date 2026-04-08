d0c8de17bb5a26f6f20f1ec27ab5e70e91cd2782
Claudia <yamiunow@gmail.com>
2026-04-07 21:04:12 -0400

    update

--- SEMANA 3/script.js
+++ SEMANA 3/script.js
@@ -1,8 +1,12 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Evita que la página se recargue
 
-    const nombre = document.getElementById('nombre').value;
+    // 1. Referencias a elementos
+    const form = this;
+    const btn = form.querySelector('button');
     const feedback = document.getElementById('formFeedback');
+    const formData = new FormData(form);
+    const nombre = formData.get('nombre') || 'amigo/a'; // Uso de FormData es más limpio
 
     // Simulación de envío dinámico
     feedback.innerHTML = `<p style="color: green; text-align: center; margin-top: 10px;">
